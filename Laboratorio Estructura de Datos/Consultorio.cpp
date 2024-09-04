01cc6a9f8cb971307086afd5bf64388e3ab64036
Wilberth Yucra <wilberth72953@gmail.com>
2024-09-04 16:30:40 -0400

    Primer avance del laboratorio

--- Laboratorio Estructura de Datos/Consultorio.cpp
+++ Laboratorio Estructura de Datos/Consultorio.cpp
@@ -1,11 +1,34 @@
+
 #include<iostream>
 #include<queue>
+#include<string>
 
 using namespace std;
 
 int main() {
-	int n;
-	int m;
-	
+
+    queue<std::string> nombres; // Vector para almacenar los nombres
+    string nombre;
+
+    cout << "\tCONSULTORIO DR. SIMI" << endl;
+    cout << "Lista de pacientes por orden de llegada: " << endl;
+
+    while (true) {
+    getline(cin, nombre);
+        if (nombre.empty()) {
+            break; // Salir del bucle si se ingresa una línea vacía
+        }
+    nombres.push(nombre); // Agregar el nombre al vector
+    }
+
+    cout << "\nNombres almacenados (en orden de ingreso):\n";
+    while (!nombres.empty()) {
+        cout << "- " << nombres.front() << "\n";
+        nombres.pop(); // Eliminar el nombre de la cola
+    }
+
+
+
 	return 0;
 }
+
